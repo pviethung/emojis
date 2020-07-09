a243486ec737aed0da7e422824e2de5d1e42450b
hungxu <pv2hung97@gmail.com>
2020-07-09 22:40:05 +0700

    Updates

---
+++ precache-manifest.edc7b915d0ef55542e7e364b1e0f07b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aaaa071194ec8fe17035bffa8f9df63",
+    "url": "/emojis/index.html"
+  },
+  {
+    "revision": "861b5abb03d45bf9e6f5",
+    "url": "/emojis/static/css/main.5f98db55.chunk.css"
+  },
+  {
+    "revision": "31f029f195cdad6b29b3",
+    "url": "/emojis/static/js/2.0a265a10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/emojis/static/js/2.0a265a10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "861b5abb03d45bf9e6f5",
+    "url": "/emojis/static/js/main.65548e3a.chunk.js"
+  },
+  {
+    "revision": "d28ea8c71a694b71a709",
+    "url": "/emojis/static/js/runtime-main.c1cbf1e3.js"
+  }
+]);
